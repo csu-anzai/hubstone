ad5dd162c4d83ec66b392862b87a3e8d487e000d
Alexandre Coulier <51952642+AlexandreCoulier@users.noreply.github.com>
2019-08-27 12:01:13 +0200

    Update 20190827091341_add_columns_to_appartements.rb

--- db/migrate/20190827091341_add_columns_to_appartements.rb
+++ db/migrate/20190827091341_add_columns_to_appartements.rb
@@ -1,11 +1,11 @@
 class AddColumnsToAppartements < ActiveRecord::Migration[5.2]
   def change
     add_column :appartements, :lot, :string
-    add_column :appartements, :étage, :string
+    add_column :appartements, :etage, :string
     add_column :appartements, :typologie, :string
-    add_column :appartements, :disponibilité, :string
+    add_column :appartements, :disponibilite, :string
     add_column :appartements, :promoteur, :string
     add_column :appartements, :logo_promoteur, :string
-    add_column :appartements, :département, :integer
+    add_column :appartements, :departement, :integer
   end
 end
