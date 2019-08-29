c8f18ba2ca959eb6ee0fba1b067f8c1efda7e0ce
Alan Gazielly <alan@gazielly.com>
2019-08-29 18:41:28 +0200

    Setup of Create and Delete button on card component with impact on selection index.

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,6 +1,28 @@
 class SelectionsController < ApplicationController
-
   def index
     @selections = Selection.where(user: current_user)
+  end
+
+  def create
+    @selection = Selection.new
+    @selection.user = current_user
+    @selection.appartement = Appartement.find(params[:appartement_id])
+    if @selection.save
+      redirect_to appartements_path
+    else
+      render 'pages/home'
+    end
+  end
+
+  def destroy
+    @selection = Selection.find(params[:id])
+    @selection.destroy
+    redirect_to selections_path
+  end
+
+  private
+
+  def selection_params
+    params.require(:selection).permit(:user_id, :appartement_id)
   end
 end
