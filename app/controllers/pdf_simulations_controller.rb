4f38059381a4b8e8cfd235b6c823e1d28748a0ea
Alan Gazielly <alan@gazielly.com>
2019-09-03 18:39:56 +0200

    PDF generation including frame in table.

--- app/controllers/pdf_simulations_controller.rb
+++ app/controllers/pdf_simulations_controller.rb
@@ -5,7 +5,13 @@ class PdfSimulationsController < ApplicationController
       format.pdf do
         render pdf: "simulation", # Excluding ".pdf" extension.
                template: "pdf_simulations/show.pdf.erb",
-               layout: "pdf.html.erb"
+               layout: "pdf.html.erb",
+               margin: {
+                 top: 20, # default 10 (mm)
+                 bottom: 20,
+                 left: 20,
+                 right: 20
+               }
       end
     end
   end
